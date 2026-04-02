cfff371e0c8c7c1000957dfdee62434a883fffc2
Shikha5410 <shikhakushwaha712@gmail.com>
2026-04-02 09:02:58 +0530

    added total pairs count in arrays

--- Arrays/PairsInArray.java
+++ Arrays/PairsInArray.java
@@ -1,14 +1,17 @@
 public class PairsInArray{
     public static void printpairs(int arr[]){
         int n = arr.length;
+        int totalpairs = 0;
 
         for(int i=0;i<n;i++){
             int current = arr[i];
             for(int j=i+1; j<n; j++){
                 System.out.print("(" + current + "," + arr[j] + ")");
+                totalpairs++;
             }
             System.out.println();
         }
+        System.out.print("Total pairs :"+ totalpairs);
     }
     public static void main (String args[]){
         int arr[] = {2,4,6,8,10};
