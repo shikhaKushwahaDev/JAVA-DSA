37e59294c0d0498377e09ec254eb900902f95f01
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-04-27 20:37:39 +0530

    Update LargestString.java
    Added comments to explain logic and compareTo( ) usage

--- String/LargestString.java
+++ String/LargestString.java
@@ -1,10 +1,14 @@
 public class LargestString{
+    // function to find largest string
+    // TC is O(n) and SC is O(1)
     public static void main (String args[]){
         String fruits[]={"apple","mango","banana"};
-
+        // Assume first element is largest initially
         String largest = fruits[0];
         for(int i=1;i<fruits.length;i++){
+            // compare strings using compareTo()
             if(largest.compareTo(fruits[i])< 0){
+                // update largest if bigger string found
                 largest = fruits[i];
             }
         }
