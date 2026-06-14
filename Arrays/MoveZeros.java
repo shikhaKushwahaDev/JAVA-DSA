b9874a64eefd67187b0cfb71397d8f63fb715fc4
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-06-14 20:28:57 +0530

    Update MoveZeros.java

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -1,8 +1,12 @@
 public class MoveZeros{
+    // Moves all zeroes to the end while preserving the order of non-zero elements  
+    // Time complexity :  O(n)
+    // Space complexity : O(1)
     public static void move_zeros(int arr[]){
         int j = 0; //postion for non zero element
         
         for(int i=0; i<arr.length; i++){
+            // Place non-zero elements at the front
             if(arr[i] != 0){
                 int temp = arr[i];
                 arr[i] = arr[j];
