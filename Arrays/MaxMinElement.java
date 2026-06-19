3d734c8e24b36f76917a72e6b1da7f63dea2b92d
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-06-19 19:47:43 +0530

    Update MaxMinElement.java
    Optimizations and added comments for clarity.

--- Arrays/MaxMinElement.java
+++ Arrays/MaxMinElement.java
@@ -1,19 +1,26 @@
-public class MaxMin_element{
+public class MaxMinElement{
+    
     public static void main (String args[]){
         int num[] = {2,5,1,9,3};
-
+        
+     // Assume the first element is both maximum and minimum
         int max = num[0];
         int min = num[0];
-
-        for(int i=0; i<num.length; i++){
-            if(max<num[i]){
+        
+     // Traverse the array from the second element
+        for(int i=1; i<num.length; i++){
+            // Update maximum element if a larger value is found
+            if(num[i]>max){
                 max=num[i];
             }
-            if(min>num[i]){
+            // Update minimum element if a smaller value is found
+            if(num[i]<min){
                 min=num[i];
             }
         } 
-        System.out.println("Maximum element is"+" " +max);
-        System.out.println("Minimum element is"+" "+min);
+        System.out.println("Maximum element is"+" " +max); 
+        System.out.println("Minimum element is"+" "+min); 
     }
 }
+// Time Complexity : O(n)
+// Space Complexity :O(1)
