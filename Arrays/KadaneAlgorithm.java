5afc5418bc6722debe8ea09d18c2f69c2fce329f
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-06-21 20:41:34 +0530

    Update KadaneAlgorithm.java
    Refactored Kadane's algorithm by updating maxsum before resetting currentsum ,ensuring correct results for all-negative arrays.

--- Arrays/KadaneAlgorithm.java
+++ Arrays/KadaneAlgorithm.java
@@ -1,14 +1,18 @@
 public class KadaneAlgorithm{
+    // Function to Find the maximum subarray sum
     public static void kadane_algorithm(int arr[]){
         int cursum = 0;
         int Maxsum = Integer.MIN_VALUE;
-        
+
+        // Traverse the array
         for(int i=0;i<arr.length;i++){
-            cursum = cursum + arr[i];
+            cursum = cursum + arr[i]; // Add current element
+            Maxsum = Math.max(cursum,Maxsum);  // Update maximum sum
+
+            // If current sum becomes negative ,reset it 
             if(cursum<0){
                 cursum = 0;
             }
-            Maxsum = Math.max(cursum,Maxsum);
         }
         System.out.println("Maxsum is" +" "+ Maxsum);
     }
@@ -17,3 +21,5 @@ public class KadaneAlgorithm{
         kadane_algorithm(arr);
     }
 }
+// Time Complexity:O(n)
+// Space Complexity:O(1)
