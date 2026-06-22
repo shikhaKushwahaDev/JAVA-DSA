70cfd4490f0c2a43a58fe8adcd47a8dee6fce6cb
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-06-22 20:25:31 +0530

    Update BubbleSort.java
    Add modified Bubble sort implementation

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,17 +1,32 @@
+/** Problem : Bubble Sort
+
+    Time Complexity:
+    Best:O(n)
+    worst:O(n^2)
+    
+    Space Complexity:O(1)
+    */
 public class BubbleSort{
-    public static void bubble_sort(int arr[]){
+    public static void modifiedBubble_sort(int arr[]){
         int n = arr.length;
-        
+
+        // Outer loop for passes 
         for(int i=0;i<n-1;i++){
+            boolean swapped = false; // Track if any swap happens
             
-            for(int j=0;j<n-i-1;j++){
+             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
-                    
+                    // Swap elements
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+
+                    swapped=true;
                 }
             }
+            // Stop if array is already sorted
+            if(swapped==false){
+                break;
         }
     }
     public static void printArr(int arr[]){
@@ -22,7 +37,7 @@ public class BubbleSort{
     }
     public static void main (String args[]){
         int arr[] = {5,4,1,3,2};
-        bubble_sort(arr);
+        modifiedBubble_sort(arr);
         printArr(arr);
     }
 }
