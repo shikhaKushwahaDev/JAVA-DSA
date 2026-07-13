da4a3369bff46e7f8af5a2c4df522aa3c9e7a015
Shikha Kushwaha <shikhakushwaha712@gmail.com>
2026-07-13 15:42:22 +0530

    Update RotateArray.java
    Added complexity analysis, comments and main () method.

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -1,12 +1,22 @@
- public class RotateArray{
+/* Problem: Rotate Array
+
+ Approach : Reverse he entire array,then reverse the first k elements and the remaining elements
+
+ Time Complexity : O(n)
+ Space Complexity : O(1)
+*/
+public class RotateArray{
     public static void rotate(int[] nums, int k) {
-        int n= nums.length;
+        int n = nums.length;
+     
+      // Handle cases where k is greater than the array length
         k=k%n;
 
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
     }
+   // Function to reverse a part of the array
     public static void reverse(int nums[],int start,int end){
         while(start<end){
             int temp=nums[start];
@@ -16,5 +26,13 @@
             end--;
         }
     }
-    
+    public static void main(String args[]){
+        int nums[] = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums,k);
+
+        for(int num : nums){
+          System.out.print(num+" ");
+        }
+    }
 }
